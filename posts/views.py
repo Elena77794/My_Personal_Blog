aa6afb13a41851061cecb60b153896ccf1e9b50d
Admin <admin@email.com>
2024-10-29 13:10:36 +0700

    make responsive contact.html

--- posts/views.py
+++ posts/views.py
@@ -4,11 +4,11 @@ from django.shortcuts import render
 import requests
 
 
-#Create your views here.
+# Create your views here.
 def home(request):
     response = requests.get('https://api.npoint.io/c790b4d5cab58020d391').json()
-    data = { "posts": response
-    }
+    data = {"posts": response
+            }
     return render(request, "posts/index.html", data)
 
 
@@ -25,5 +25,13 @@ def show_post(request, post_id):
 def about_page(request):
     return render(request, "posts/about.html")
 
+
 def contact_page(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone_number = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f"Come from {name}, mail_box {email}, phone_number: {phone_number} with message {message}")
+        return  HttpResponse("Succesfully sent your message")
     return render(request, "posts/contact.html")
